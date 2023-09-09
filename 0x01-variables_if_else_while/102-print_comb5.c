03577a73f12ceee2116e6f384107311167a29ab9
karamagueye <karamagueye04@gmail.com>
2023-09-09 10:52:22 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,16 +10,16 @@
  */
 int main(void)
 {
-            
+    
 	int num1, num2;
 
-            
+    
 	for (num1 = 0; num1 <= 99; num1++)
     
 	{
-            
-	for (num2 = num1; num2 <= 99; num2++)
-    
+        
+	for (num2 = num1 + 1; num2 <= 99; num2++)  // Start from num1 + 1
+        
 	{
             
 	putchar((num1 / 10) + '0');
@@ -33,24 +33,24 @@ int main(void)
 	putchar((num2 % 10) + '0');
 
             
-	if (num1 != 99 || num2 != 99)
-    
+	if (num1 != 98 || num2 != 99)
+            
 	{
-           
+                
 	putchar(',');
-           
+                
 	putchar(' ');
-    
+            
 	}
-    
+        
 	}
     
 	}
 
-         
+    
 	putchar('\n');
 
-          
+    
 	return (0);
 }
 
