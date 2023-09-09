f4fbfad855ea6d517b1e448b8171661c27578013
karamagueye <karamagueye04@gmail.com>
2023-09-09 06:50:03 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,12 +13,12 @@ int main(void)
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
-		putchar(i);
+	putchar(i);
 
 	putchar('\n');
 
 	for (i = 'A'; i <= 'Z'; i++)
-		putchar(i);
+	putchar(i);
 
 	putchar('\n');
 
