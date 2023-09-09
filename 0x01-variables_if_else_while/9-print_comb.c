cbcd0e491fbb42dcf74397600ffe3c50ff0ee67d
karamagueye <karamagueye04@gmail.com>
2023-09-09 09:04:56 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,15 +12,18 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0, num <= 9, num++)
+
 	{
 	putchar(num + '0');
 
 	if (num < 9)
-		{
-		putchar(',');
-		putchar(' ');
+	{
+	putchar(',');
+
+	putchar(' ');
 	}
+
 	}
 
 	putchar('\n');
