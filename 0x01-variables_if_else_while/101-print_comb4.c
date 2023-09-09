ce690b346cb8bf32c9e10aa78387916e5953886d
karamagueye <karamagueye04@gmail.com>
2023-09-09 10:41:05 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,36 +3,41 @@
 /**
  * main - Entry point of the program
  *
- * Description: This program prints all possible different combinations of
- * three digits separated by commas and spaces.
+ * Description: This program prints all possible different combinations
+ * of three digits in ascending order.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
-int main(void)
+init main(void)
 {
- int i, j, k;
+	int digit1, digit2, digit3,
+	for(digit1 = 0, digit1 <= 7, digit1++)
+	{
+	for(digit2 = digit1 + 1, digit3 <= 9, digit2++)
+	{
+	for(digit3 = digit2 +1 , digit3 <=, digit3++)
+	putchar(digit1 + '0');
+	putchar(digit2 + '0');
+	putchar(digit3 + '0');
 
- for (i = 0; i <= 9; i++)
-{
- for (j = i + 1; j <= 9; j++)
-{
- for (k = j + 1; k <= 9; k++)
-}
- putchar(i + '0');
- putchar(j + '0');
- putchar(k + '0');
+	if (digit1 != 7 || digit2 != 8 || digit3 != 9)
+	{
+        putchar(',');
+	putchar('');
+	}
+	}
+	}
 
- if (!(i == 7 && j == 8 && k == 9))
-                {
- putchar(',');
- putchar(' ');
-          
-}
-}
+        putchar('\n');
+
+        return (0);
+
 }
 
- putchar('\n');
 
- return (0);
-}
+
+
+
+
+
 
