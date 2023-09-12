15a04f103621532939d1d70f71820ec4765f1028
karamagueye <karamagueye04@gmail.com>
2023-09-12 08:16:51 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is the putchar function
+ *Description: the code line use printf to print putchar.
+ */
+int main(void)
+{	printf("_putchar \n");
+
+	return (0);
+}
