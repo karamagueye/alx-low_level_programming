e577ae9a67c8c6684da6cbdd933947927e3474d2
karamagueye <karamagueye04@gmail.com>
2023-09-15 18:52:34 -0700

    3-print_remaining_days.c

---
+++ 0x03-debugging/convert_day.h
@@ -0,0 +1,12 @@
+#ifndef CONVERT_DAY_H
+#define CONVERT_DAY_H
+
+// Déclaration d'une fonction
+int convert_day(int month, int day, int year);
+
+// Déclaration de constantes
+#define DAYS_IN_WEEK 7
+#define MONTHS_IN_YEAR 12
+
+#endif /* CONVERT_DAY_H */
+
