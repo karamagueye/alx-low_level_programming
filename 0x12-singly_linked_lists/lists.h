31edc129cecb33c1c03ccf742a240aa96e0129d8
karama gueye <karamagueye04@gmail.com>
2023-10-20 09:37:13 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -9,6 +9,7 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
     char *str;
@@ -21,6 +22,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
 
 #endif
