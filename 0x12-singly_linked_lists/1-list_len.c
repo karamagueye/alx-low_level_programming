824720a1e14d026667ef9e3556a30b59f4ed8ce2
karama gueye <karamagueye04@gmail.com>
2023-10-20 08:04:05 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,17 +2,20 @@
 #include "lists.h"
 
 /**
- *  function that adds a new node at the beginning of a list_t
+ *  list_len - adds a new node at the beginning of a list_t
+ *  @h: pointer to the list_t list
  *  Return: the address of the new element, or NULL if it failed
  */
 size_t list_len(const list_t *h)
 {
 	size_t n = 0;
+
 	while (h)
 	{
 		n++;
 		h = h->next;
 	}
 	return (n);
-	
+
+
 }
