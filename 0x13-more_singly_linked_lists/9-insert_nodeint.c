aa3eb8740e0068df33821dea77d9e956fbbc37d7
karama gueye <karamagueye04@gmail.com>
2023-10-23 19:31:58 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,10 +29,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 	for (i = 0; temp && i < idx; i++)
-	
+	{
+
 		if (i == idx - 1)
 		{
-
 	new->next = temp->next;
 	temp->next = new;
 	return (new);
@@ -40,6 +40,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 else
 temp = temp->next;
+	}
 return (NULL);
 }
 
