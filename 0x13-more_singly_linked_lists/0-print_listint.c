0160786e560cf88febcf404e763f120f24a99bb8
karama gueye <karamagueye04@gmail.com>
2023-10-25 12:53:40 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,9 +2,9 @@
 
 /**
  * print_listint-prints all the elements of a linked list
- * @h: linked list of type listint_t to print
+ * @h:linked list of type listint_t to print
  *
- * Return: number of nodes
+ * Return:number of nodes
  */
 
 size_t print_listint(const listint_t *h)
